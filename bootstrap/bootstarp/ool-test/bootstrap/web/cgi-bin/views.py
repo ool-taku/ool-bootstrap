70ec7cbc1d82484358635651a9f9453b47ad403b
kurahashiryo <kurahashi-rxa@necst.nec.co.jp>
2014-02-24 17:57:22 +0900

    remove junk file

--- bootstrap/bootstarp/ool-test/bootstrap/web/cgi-bin/views.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-from django.http import HttpResponse
-from django.template import Context, loader
-
-def index(request):
-	context = Context()
-	t = loader.get_template('index.html')
-	return HttpResponse(t.render(context))
--- bootstrap/bootstarp/ool-test/bootstrap/web/static/js/ajax.js
+++
@@ -1,8 +0,0 @@
-new Ajax.Request(
-　　　　　"cgi-bin/ajax.py",
-　　　　　{ method: 'get',  onComplete: displayData }); 
-
-function displayData(responseHttpObj) {
-	//alert(responseHttpObj.responseText);
-	document.getElementById("test2").innerHTML = responseHttpObj.responseText;
-} 
